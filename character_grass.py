97b5fb88e6f000f9a80e9293739c604e50310de6
neneee <2022180024@tukorea.ac.kr>
2024-09-24 12:12:15 +0900

    update: 파일 삭제 변경 및 원 운동 구현

---
+++ character_grass.py
@@ -0,0 +1,36 @@
+from pico2d import *
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+
+def run_circle():
+    print("CIRCLE")
+
+    r, cx, cy = 300, 800 // 2, 600 // 2
+
+    for degree in range(0, 360, 3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+        clear_canvas_now()
+        boy.draw_now(x, y)
+        delay(0.1)
+
+    pass
+
+
+def run_rectangle():
+    print("RECTANGLE")
+    pass
+
+
+# fill here
+while True:
+    run_rectangle()
+    run_circle()
+
+close_canvas()
