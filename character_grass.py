97af1d8fe63f03cc5311999ee3a619db25294646
neneee <2022180024@tukorea.ac.kr>
2024-09-24 12:45:02 +0900

    update: 원 사각형 성공 완료

--- character_grass.py
+++ character_grass.py
@@ -6,33 +6,45 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x,y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    boy.draw_now(x, y)
+    delay(0.1)
+    pass
 
 def run_circle():
     print("CIRCLE")
 
-    r, cx, cy = 300, 800 // 2, 600 // 2
+    r, cx, cy = 210, 800 // 2, 600 // 2
 
     for degree in range(0, 360, 3):
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
-        clear_canvas_now()
-        boy.draw_now(x, y)
-        delay(0.1)
+        draw_boy(x,y)
 
     pass
 
 def run_top():
     print("TOP")
+    for x in range (0,800,10):
+        draw_boy(x,550)
     pass
 def run_right():
+    for y in range (550,90,-10):
+        draw_boy(790,y)
     print("RIGHT")
     pass
 def run_bottom():
     print("BOTTOM")
+    for x in range (800,0,-10):
+        draw_boy(x,90)
     pass
 def run_left():
     print("LEFT")
+    for y in range (90, 550,10):
+        draw_boy(0,y)
     pass
 
 def run_rectangle():
@@ -46,7 +58,7 @@ def run_rectangle():
 
 # fill here
 while True:
-    run_rectangle()
     run_circle()
+    run_rectangle()
 
 close_canvas()
