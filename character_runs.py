a1ba7dc66bd15ccaec39d1421c9ce97c47c32d2e
neneee <2022180024@tukorea.ac.kr>
2024-09-24 11:52:58 +0900

    feat: 파일 추가 및 함수 생성

---
+++ character_runs.py
@@ -0,0 +1,22 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('run_animation.png')
+
+def run_circle():
+    print("CIRCLE")
+    pass
+
+def run_rectangle():
+    print("RECTANGLE")
+    pass
+
+# fill here
+while True:
+    run_rectangle()
+    run_circle()
+
+close_canvas()
+
