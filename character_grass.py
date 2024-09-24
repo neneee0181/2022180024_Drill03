9d700bfc9dffb63afc23a972bd6ae6e31b50a7d0
neneee <2022180024@tukorea.ac.kr>
2024-09-24 12:24:12 +0900

    update: rectangle에 필요한 함수 추가

--- character_grass.py
+++ character_grass.py
@@ -22,9 +22,25 @@ def run_circle():
 
     pass
 
+def run_top():
+    print("TOP")
+    pass
+def run_right():
+    print("RIGHT")
+    pass
+def run_bottom():
+    print("BOTTOM")
+    pass
+def run_left():
+    print("LEFT")
+    pass
 
 def run_rectangle():
     print("RECTANGLE")
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
     pass
 
 
